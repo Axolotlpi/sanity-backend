f61d4388f09a14e349d20298bb0a08054b38a997
Axolotlpi <cfc3.1415@gmail.com>
2021-11-02 21:40:11 -0500

    explain script

--- generate.sh
+++ generate.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-cd ../astro-frontend/
+#frontend directory
+cd ../astro-frontend/ 
+
+#your fontend build script, and it's parameters
 ./buildToServer.sh ../sanity-backend/public/
+
+#back to your backend
 cd ../sanity-backend
+
+#backup your newly generated frontend
 rm -r ./public-backup/*
 cp -r ./public/* ./public-backup/
